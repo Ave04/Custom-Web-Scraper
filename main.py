431ce7a5e8a15ba3eae495e2d07cbbb1d7dbaca7
Avish Madhow <avish.madhow@gmail.com>
2025-01-29 12:22:29 +0000

    dataframe checked and cleaned if needed

--- main.py
+++ main.py
@@ -6,12 +6,36 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from time import sleep
 import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+from textblob import TextBlob
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+# Clean and preprocess text
+def clean_text(text):
+    # Remove special characters (but keep numbers)
+    text = re.sub(r'[^\w\s]', '', text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    filtered_text = [word for word in word_tokens if word not in stop_words]
+
+    return ' '.join(filtered_text)
+
 
 url_site = 'https://www.forbes.com/'
 
 response = requests.get(url=url_site)
 soup = BeautifulSoup(response.text, 'html.parser')
-
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -34,4 +58,9 @@ driver.quit()
 
 df = pd.DataFrame(article_data)
 df.to_csv('forbes_articles.csv', index=False)
+
+df.columns = ["title"]  # Assign a proper column name
+print(df.head())  # Check the structure
+
+# df["cleaned_title"] = df["title"].apply(clean_text)
 
